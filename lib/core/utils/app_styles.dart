20410f0d643c4e881385ff51d5a086546eff03f9
FakhrElddin <fakhereldeanaymanq@gmail.com>
2026-06-07 07:49:49 +0300

    create app styles class

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,26 @@
+import 'package:cure_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyles {
+  static TextStyle bold26Text = GoogleFonts.openSans(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+  static TextStyle semiBold22Text = GoogleFonts.openSans(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    color: AppColors.blackColor,
+  );
+  static TextStyle regular18Text = GoogleFonts.openSans(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: AppColors.greyColor,
+  );
+  static TextStyle semiBold18Text = GoogleFonts.openSans(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: AppColors.blackColor,
+  );
+}
